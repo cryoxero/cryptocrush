3dbe338056f205cda8eb710e85d4b71c6b010bad
cryoxero <cryoxero@proton.me>
2025-05-30 20:19:59 +0330

    Add Posts ORM and superuser

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
