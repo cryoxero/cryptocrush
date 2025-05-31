7d12268fe424a15c82443b5ba3439fe287346a70
cryoxero <cryoxero@proton.me>
2025-05-31 13:17:24 +0330

    We have proper posts now babyyy

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import posts_page
+from .views import posts_page, posts_welcome
 
 app_name = "posts"
 
 urlpatterns = [
-    path('<slug:slug>', posts_page, name="posts")
+    path('', posts_welcome, name="posts_welcome"),
+    path('<slug:slug>', posts_page, name="posts"),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -4,5 +4,9 @@ from .models import Post
 def posts_page(req, slug):
     post_list = Post.objects.values("title", "slug")
     curr_post = Post.objects.get(slug=slug)
-        
+    return render(req, 'posts/posts_index.html', {"post_list": post_list, "curr_post": curr_post})
+
+def posts_welcome(req):
+    post_list = Post.objects.values("title", "slug")
+    curr_post = Post.objects.get(slug="welcome")
     return render(req, 'posts/posts_index.html', {"post_list": post_list, "curr_post": curr_post})
